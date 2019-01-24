d158fc4131d1d5c36f3d320245a0a0e7f1bc3ea7
EiKEUFS <eike.brito@dcomp.ufs.br>
2019-01-24 10:22:55 -0300

    Criando os Models e Migrates

---
+++ database/migrations/2019_01_24_120724_create_adocaos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdocaosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adocaos', function (Blueprint $table) {
+            $table->increments('id_adocao');
+            $table->integer('id_adotante')->unsigned();            
+            $table->integer('id_animal')->unsigned();
+            $table->date('data_adocao');
+            $table->timestamps();
+
+            $table->foreign('id_adotante')->references('id_adotante')->on('adotantes')->onDelete('cascade');
+            $table->foreign('id_animal')->references('id_animal')->on('animals')->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adocaos');
+    }
+}
