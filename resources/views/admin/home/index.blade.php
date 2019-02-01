8bb6d983b82728c13aa9e0e2e77ab7d05ad022c5
joaomsal <jmsalmeida@dcomp.ufs.br>
2019-02-01 15:21:32 -0300

    Edição da página home

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -20,6 +20,8 @@
         
 <div>
     <div class="slide-one-item home-slider owl-carousel">
+      <div class="site-blocks-cover" style="background-image: url(images/home.png);" data-aos="fade" data-stellar-background-ratio="0.5">
+      </div>
       <div class="site-blocks-cover" style="background-image: url(images/gato.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
       </div>
 
@@ -59,34 +61,8 @@
     </div> <!-- .block-feature -->
 
 
-      <div class="callout callout-danger" style="padding: 5%;text-align: center;">
-        <div class="box-body">
-          <div>
-            <h2 class="mb-3 text-uppercase">QUEM <strong>SOMOS</strong>?</h2>
-            <p class="lead">A <strong>BICHOS DO CAMPUS</strong> é text text text
-            text text text tex  text tex te tet x et xtet </p>
-          </div>
-        </div>
-      </div>
-
-
-      <div class="callout callout-success" style="padding: 5%; font-size: 18px;text-align: center;">
-        <div class="box-body">
-            <h2 class="mb-3 text-uppercase">O QUE EU PRECISO PARA <strong>ADOTAR</strong>?</h2>
-            <p>A Bichos do Campus tem um processo de adoção seguindo alguns critérios.</p>
-            <p>Este processo é feito para garantir o conforto do animal e evitar possíveis problemas.</p>
-            <ul>
-              <li>É preciso não ter histórico de abandono.</li>
-              <li>Caso você more em apartamento, é necessário ter as janelas teladas.</li>
-              <li>Outros.</li>
-            </ul></br>
-            <button class="btn bg-orange">QUERO ADOTAR</button>
-          
-        </div>
-      </div>
-
-<div class="box-body">
-    <h1>COMO ADOTAR?</h1>
+<div class="box-body" style="text-align: center;">
+    <h2>COMO <strong>ADOTAR?</strong></h2>
         <div class="col-md-3 col-sm-6 col-xs-12">
           <div class="info-box">
             <span class="info-box-icon bg-aqua"><i class="fa fa-paw"></i></span>
@@ -137,6 +113,33 @@
         <!-- /.col -->
       
   </div>
+
+      <div class="callout callout-danger" style="padding: 5%;text-align: center;">
+        <div class="box-body">
+          <div>
+            <h2 class="mb-3 text-uppercase">QUEM <strong>SOMOS</strong>?</h2>
+            <p class="lead">A <strong>BICHOS DO CAMPUS</strong> é text text text
+            text text text tex  text tex te tet x et xtet </p>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="callout callout-success" style="padding: 5%; font-size: 18px;text-align: center;">
+        <div class="box-body">
+            <h2 class="mb-3 text-uppercase">O QUE EU PRECISO PARA <strong>ADOTAR</strong>?</h2>
+            <p>A Bichos do Campus tem um processo de adoção seguindo alguns critérios.</p>
+            <p>Este processo é feito para garantir o conforto do animal e evitar possíveis problemas.</p>
+            <ul>
+              <li>É preciso não ter histórico de abandono.</li>
+              <li>Caso você more em apartamento, é necessário ter as janelas teladas.</li>
+              <li>Outros.</li>
+            </ul></br>
+            <button class="btn bg-orange">QUERO ADOTAR</button>
+          
+        </div>
+      </div>
+
 
     <footer class="site-footer">
       <div class="container">
