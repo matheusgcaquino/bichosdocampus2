1594fb1376c7f0024a29746e41fd951484ec39ae
juninho truesdale <jmsalmeida@dcomp.ufs.br>
2019-03-20 15:02:45 -0300

    Alterando front-end

--- resources/views/adoções/publico/requisição/index.blade.php
+++ resources/views/adoções/publico/requisição/index.blade.php
@@ -178,7 +178,7 @@
 
 div img {
   max-width: 100%;
-  height: 100%;
+  max-height: 100%;
  
 }
 </style>
--- resources/views/adoções/restrito/requisições/index.blade.php
+++ resources/views/adoções/restrito/requisições/index.blade.php
@@ -87,7 +87,7 @@
 
 div img {
  max-width: 100%;
-  height: 100%;
+  max-height: 100%;
  
 }
 </style>
