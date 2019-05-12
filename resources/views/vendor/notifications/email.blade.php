9784e61420bf3693ce3cf528f02e83319a2d8482
Matheus Aquino <matheus.aquino@dcomp.ufs.br>
2019-05-12 20:26:11 -0300

    - Atualização do env-examplo;
    - Envio de todos os e-mail tem como blade o mesmo arquivo;
    
    - E-mail de reset de senha corrigido.

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,12 +1,22 @@
+@php
+    if ($actionText == "Reset Password"){
+        $actionText = "Redefinir senha";
+        $introLines = ['Você está recebendo este e-mail porque recebemos uma 
+        solicitação de redefinição de senha para sua conta.'];
+        $outroLines = ['Se você não solicitou uma redefinição de senha, nenhuma 
+        ação adicional é necessária.'];
+    }
+@endphp
+
 @component('mail::message')
 {{-- Greeting --}}
 @if (! empty($greeting))
 # {{ $greeting }}
 @else
 @if ($level == 'error')
-# Whoops!
+# Oops!
 @else
-# Hello!
+# Olá!
 @endif
 @endif
 
@@ -45,14 +55,14 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+Saudações,<br>{{ config('app.name') }}
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({!! $actionUrl !!})
+Se você estiver com problemas para clicar no botão de "{{ $actionText }}" 
+copie e cole o URL abaixo em seu navegador da web: [{{ $actionUrl }}]({!! $actionUrl !!})
 @endcomponent
 @endisset
 @endcomponent
