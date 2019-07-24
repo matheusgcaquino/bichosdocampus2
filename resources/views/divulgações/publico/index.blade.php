4ede40ec2b19b75bcd2e63774ce416f17c81482a
Matheus Aquino <matheus.aquino@dcomp.ufs.br>
2019-07-24 20:07:43 -0300

    Criando base para cancelar inscrições

---
+++ resources/views/divulgações/publico/index.blade.php
@@ -0,0 +1,35 @@
+@extends('adminlte::page')
+
+@section('title', 'Inscrição - Bichos do Campus')
+
+@section('content_header')
+@stop
+
+@section('content')
+
+  <!-- Mensagem de Alerta -->
+  @include('site.includes.alerts')
+
+  <div class="box">
+
+    <div class="box-header">
+      <h3>
+        <center>
+          Cancelar <strong>Inscrição</strong>
+        </center>
+      </h3>
+ 
+
+    <div class="box-body">
+      <div class="alert alert-warning">
+        <h3>
+            <center>
+              <i class="fa fa-wrench"></i>
+              Em construção!
+            </center>
+        </h3>
+      </div>
+    </div>
+    
+  </div>  
+@stop
